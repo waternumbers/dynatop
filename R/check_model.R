9669d1dd60a7f138db6071b372f1044ed06e00b6
Paul Smith <paul@waternumbers.co.uk>
2019-06-18 08:43:14 +0100

    Building and with improved initialisation
    Former-commit-id: ff01b6f5274074d984277c3ad4f3f0eeb5ec2adc

--- R/check_model.R
+++ R/check_model.R
@@ -11,32 +11,37 @@ check_model <- function(model){
 
     ## First perform checks that rely only on individual components of the model
     
-    ## checks only on the HRU table
-    if(!is.data.frame(model$hru)){ # check HRU is a data frame
-        stop("HRU table should be a data.frame")
+    ## checks only on the HRU tables
+    if(!is.data.frame(model$hillslope) | !is.data.frame(model$channel)){ # check HRU is a data frame
+        stop("HRU tables should be a data frames")
     }else{
-        ## columns of parameters and the type of the contents 
-        hru_parameter_columns <- c(srz_max="character",
-                                   srz_0="character",
-                                   ln_t0="character",
-                                   m="character",
-                                   td="character",
-                                   tex="character")
-        ## all required columns in the HRU table and type of the contents
-        hru_req_columns <- c(id="numeric",
-                             type="character",
-                             area="numeric",
-                             atb_bar='numeric',
-                             precip_input="character",
-                             pet_input="character",
-                             hru_parameter_columns)
-        
-        if(!all(names(hru_req_columns) %in% names(model$hru))){ # check it has required columns
-            stop( paste(c("HRU table missing columns:",setdiff(names(hru_req_columns), names(model$hru))),collapse=" ") )
-        }else{ ## check data types
-            tmp <- sapply(model$hru,class) # types of the columns
-            if( any( tmp[names(hru_req_columns)] != hru_req_columns ) ){
-                stop( paste(c("Incorrect types in HRU table columns:",names(hru_req_columns)[which(tmp[names(hru_req_columns)] != hru_req_columns)]),collapse=" ") )
+        ## make data frames of columns expected in each table, the type of value expected and if they are a parameter
+        hru_properties <- list(
+            hillslope = data.frame(name = c("id","area","atb_bar","s_bar",
+                                            "precip_input","pet_input",
+                                            "srz_max","srz_0","ln_t0","m","td","tex"),
+                                   type=c(rep("numeric",4),rep("character",8)),
+                                   is_param <- c(rep(FALSE,6),rep(TRUE,6)),
+                                   stringsAsFactors=FALSE),
+            channel = data.frame(name = c("id","area","precip_input","pet_input"),
+                                 type=c(rep("numeric",2),rep("character",2)),
+                                 is_param <- rep(FALSE,4),
+                                 stringsAsFactors=FALSE)
+        )
+
+        ## check just the HRU tables
+        for(ii in names(hru_properties)){
+            idx <- hru_properties[[ii]]$names %in% names(model[[ii]])
+            if( !all( idx ) ){# check it has required columns
+                stop( paste(c("HRU table",ii,"missing columns:",
+                              hru_properties[[ii]]$names[!idx]),collapse=" ") )
+            }else{ ## check data types
+                tmp <- sapply(model[[ii]],class) # types of the columns
+                idx <- tmp[ hru_properties[[ii]]$names ] != hru_properties[[ii]]$type
+                if( any( idx ) ){
+                    stop( paste(c("Incorrect types in HRU table",ii,"columns:",
+                                  hru_properties[[ii]]$names[idx]),collapse=" ") )
+                }
             }
         }
     }
@@ -62,26 +67,30 @@ check_model <- function(model){
     ## END of checks on individual objects return if failed
 
     ## check unique ids
-    if( length(unique(model$hru[,'id'])) < nrow(model$hru) ){
-        stop("HRU ids should be unique")
-    }
-
-    ## check HRUs are recognised types
-    if( !all(model$hru[,'type'] %in% c('hillslope','channel')) ){
-        stop("HRU types should be hillslope or channel (case sensitive)")
+    for(ii in c("hillslope","channel")){
+        if( length(unique(model[[ii]]$id)) < nrow(model[[ii]]) ){
+            stop(paste(ii,"HRU ids should be unique"))
+        }
     }
 
     ## check parameters
-    tmp <- unique( unlist(model$hru[,names(hru_parameter_columns)]) ) ## all unique parameter names
-    if( !all( tmp %in% names(model$param) ) ){
+    pnames <- NULL
+    for(ii in c("hillslope","channel")){
+        tmp <- hru_properties[[ii]][ hru_properties[[ii]]$is_param , 'name']
+        pnames <- c(pnames,
+                    unlist( model[[ii]][,tmp] ))
+    }
+    pnames <- unique(pnames)
+    if( !all( pnames %in% names(model$param) ) ){
         stop("Not all parameters are specified")
     }
-    if( !all( names(model$param) %in% tmp ) ){
+    
+    if( !all( names(model$param) %in% pnames ) ){
         stop("Some parameters are not used")
     }
 
     ## check Wex and Wsat
-    hillslope_hru <- as.character( model$hru[ model$hru[,'id']=='hillslope' ,'id'] )
+    hillslope_hru <- as.character( model$hillslope[,'id'] )
     for(ii in c("Wsat","Wex")){
         if( any( colnames(model[[ii]]) != rownames(model[[ii]]) ) ){
             stop(ii," should have identically ordered column and row names")
@@ -93,8 +102,8 @@ check_model <- function(model){
     }
 
     ## check Fex and Fsat
-    channel_hru <- as.character( model$hru[ model$hru[,'id']=='channel' ,'id'] )
-    for(ii in c("Wsat","Wex")){
+    channel_hru <- as.character( model$channel[,'id'] )
+    for(ii in c("Fsat","Fex")){
         if( any(colnames(model[[ii]]) != hillslope_hru) ){
             stop(ii," column names should match order of hillslopes in the HRU table")
         }
