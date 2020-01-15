917bdbb4d7d1fad0140a3d0c0cc1580a3fedc066
Paul Smith <paul@waternumbers.co.uk>
2020-01-15 01:29:10 +0000

    added initial cpp functions and logic
    Former-commit-id: dd350752a9e0b4dabd761ad797fc9b0a39d7ecb8

--- leaf_and_branch/dynatop_lb.R
+++ leaf_and_branch/dynatop_lb.R
@@ -2,11 +2,12 @@
 rm(list=ls())
 library("Rcpp")
 ## leaf types
-transfers <- function(){c("precip"=0,"pet"=0,"lex"=0,"lch"=0,"qch"=0)}
+transfers <- function(){c("precip"=0,"pet"=0,"lex"=0,"lsz"=0,"qch"=0)}
 
 external <- function(id){
     list(id=id,
          type = "ex",
+         series = c("precip"="a","pet"="b"),
          output = transfers())
 }
 
@@ -28,6 +29,7 @@ mux <- function(id,us){
     list(id=id,
          type="mux",
          uptree=us,
+         weight=rep(1/length(us),length(us)),
          output=transfers())
 }
 
@@ -46,46 +48,55 @@ hillslope <- function(id,us,prop,param){
          param=param)
 }
 
-             
+channel <- function(id,us,prop){
+    list(id=id,
+         type="channel",
+         uptree=us,
+         output=transfers(),
+         prop=prop)
+}
+
+
 hru <- list(external("ex"),
-            leaf("l1","ex"),
-            mux("m2",c("ex","l1")),
-            leaf("l2","m2"),
-            branch("b1","l1",0.5),
-            mux("m1",c("b1","l2")))
+            hillslope("h1","ex",NA,NA),
+            hillslope("h2","ex",NA,NA),
+            mux("m1",c("ex","h1","h2")),
+            channel("c1","m1",c("area"=1)))
 
 names(hru) <- vapply(hru,function(x){x$id},character(1))
 
 sourceCpp("landb.cpp")
-tmp <- rcpp_dynatop(hru)
+rcpp_dynatop(hru,c("a"=4,"b"=34),list(time_step=1))
 
-hru <- rep(hru,100000)
-#system.time({
-    for(ii in names(hru)){
-        #print(ii)
-        if(hru[[ii]]$type=="ex"){
-            ##hru[[ii]] <- evolve_ex(hru[[ii]])
-            hru[[ii]]$output[] <- rnorm(5)
-        }
-        if(hru[[ii]]$type=="leaf"){
-            ## hru[[ii]] <- evolve_leaf(hru[[ii]],hru[[hru[[ii]]$uptree]]$output)
-            hru[[ii]]$output[] <- hru[[hru[[ii]]$uptree]]$output + 3
-            #for(jj in 1:20){
-            #    hru[[ii]]$output <- hru[[ii]]$output + 2
-            #}
-            
-        }
-        
-        if(hru[[ii]]$type=="branch"){
-            #hru[[ii]] <- evolve_branch(hru[[ii]],hru[[hru[[ii]]$uptree]]$output)
-            hru[[ii]]$output[] <- hru[[hru[[ii]]$uptree]]$output*hru[[ii]]$weight
-        }
-        if(hru[[ii]]$type=="mux"){
-            hru[[ii]]$output[] <- 0
-            for(jj in hru[[ii]]$uptree){
-                hru[[ii]]$output <- hru[[ii]]$output + hru[[jj]]$output
-            }
-        }
-    }
-#})
-    
+hru2 <- rep(hru,25000)
+system.time({rcpp_dynatop(hru2,c("a"=4,"b"=34))})
+## system.time({
+##     for(ii in names(hru)){
+##         #print(ii)
+##         if(hru[[ii]]$type=="ex"){
+##             ##hru[[ii]] <- evolve_ex(hru[[ii]])
+##             hru[[ii]]$output[] <- rnorm(5)
+##         }
+##         if(hru[[ii]]$type=="leaf"){
+##             ## hru[[ii]] <- evolve_leaf(hru[[ii]],hru[[hru[[ii]]$uptree]]$output)
+##             hru[[ii]]$output[] <- hru[[hru[[ii]]$uptree]]$output + 3
+##             #for(jj in 1:20){
+##             #    hru[[ii]]$output <- hru[[ii]]$output + 2
+##             #}
+
+##         }
+
+##         if(hru[[ii]]$type=="branch"){
+##             #hru[[ii]] <- evolve_branch(hru[[ii]],hru[[hru[[ii]]$uptree]]$output)
+##             hru[[ii]]$output[] <- hru[[hru[[ii]]$uptree]]$output*hru[[ii]]$weight
+##         }
+##         if(hru[[ii]]$type=="mux"){
+##             hru[[ii]]$output[] <- 0
+##             for(jj in hru[[ii]]$uptree){
+##                 hru[[ii]]$output <- hru[[ii]]$output + hru[[jj]]$output
+##             }
+##         }
+##     }
+## })
+
+## system.time({tmp <- rcpp_dynatop(hru)})
--- leaf_and_branch/landb.cpp
+++ leaf_and_branch/landb.cpp
@@ -15,20 +15,187 @@ using namespace Rcpp;
 //' @param max_index maximum value of the hillslope and channel id's
 //' @return list of hillslope and channel properties
 //'
+
+// function for external input
+void f_ex(List h, NumericVector obs){
+  //Rcout << "start ex" << "\n";
+  
+  NumericVector out = h["output"];
+  for(int j=0;j<out.length();j++){
+    out[j] = 0;
+  }
+  CharacterVector in_series = h["series"];
+  CharacterVector in_var = in_series.names();
+  for(int j=0;j<in_series.length();j++){
+    //Rcout << j << " " << in_series[j] << " " << in_var[j] << "\n";
+    std::string lbl = as<std::string>(in_series[j]);
+    std::string var = as<std::string>(in_var[j]);
+    out[var] = obs[lbl];
+  }
+  //Rcout << "leave ex" << "\n";
+};
+
+// function for mux
+void f_mux(List h, List hru){
+  CharacterVector lbl_in = h["uptree"];
+  NumericVector weight = h["weight"];
+  NumericVector out = h["output"];
+  for(int j=0;j<out.length();j++){
+    out[j] = 0;
+  }
+  for(int j=0;j<lbl_in.length();j++){
+    //Rcout << j << " " << lbl_in[j]<< "\n";
+    std::string lbl = as<std::string>(lbl_in[j]);
+    List h_in = hru[lbl];
+    NumericVector h_input = h_in["output"];
+    NumericVector w_in = weight[j]*h_input;
+    out = out + w_in;
+  }
+  // h["output"] = out;
+  //Rcout << "leave mux" << "\n";
+ };
+
+//function for channel
+void f_channel(List h, NumericVector input, List opt){
+  NumericVector output = h["output"];
+  double ts = opt["time_step"];
+  NumericVector prop = h["prop"];
+  output["qch"] = (input["lsz"] + input["lex"] + ts * prop["area"] * input["precip"])/(3600*ts);
+  //h["output"]=input;
+  //Rcout << "leave channel" << "\n";
+};
+
+double fode(double a,double b, double x0, double t){
+  double x(1);
+  if( b < 1e-10 ){b = 1e-10;}
+  x = x0*exp(-b*t) + (a/b)*exp(-b*t);
+  return x;
+}
+
+//function for hillslope
+void f_hillslope(List h, NumericVector input, List opt){
+  NumericVector prop = h["prop"];
+  NumericVector state = h["state"];
+  NumericVector param = h["param"];
+  NumericVector output = h["output"];
+  double ts = opt["time_step"];
+  double omega = opt["omega"];
+  double theta = opt["theta"];
+  
+
+  // solve surface excess
+  double ex = fode(input["lex"]/(prop["area"]*ts),
+		   1/param["tex"],
+		   state["ex"],ts);
+  double lex = ex + (input["lex"]/prop["area"]) - state["ex"];
+  state["ex"] = ex;
+  output["lex"] = lex*prop["area"];
+
+  // solve the root zone
+  double q_ex_rz = min(state["ex"]/ts, param["qex_max"]);
+  double tilde_rz = fode(input["precip"] + q_ex_rz,
+			 input["pet"]/param["srz_max"],
+			 state["rz"],ts);
+  state["rz"] = min(tilde_rz,param["srz_max"]);
+  double q_rz_ex = 0 ;
+  double q_rz_uz = tilde_rz - state["rz"];
+  if( state["sz"] <= 0 ){
+    q_rz_ex = q_rz_uz;
+    q_rz_uz = 0;
+  }
+
+  // unsaturated zone
+  double tilde_uz = fode( q_rz_uz/ts,
+			  1/(state["sz"]*param["td"]),
+			  state["uz"],ts);
+  double q_uz_sz = state["uz"] + q_rz_uz - tilde_uz;
+  state["uz"] = tilde_uz;
+
+  // saturated zone
+  double qbar = ( state["lsz"] + state["lsz_in"] + input["lsz"]
+		  + max(state["lsz"], input["lsz"]) )/4;
+  double cbar = (qbar * prop["delta_x"])/param["m"];
+  double lambda = omega + theta*cbar*ts/prop["delta_x"];
+  double lambda_prime = omega + (1-theta)*cbar*ts/prop["delta_x"];
+  double k = lambda_prime*state["lsz"] +
+    (1-lambda_prime)*min(state["lsz_in"],state["lsz_max"]) +
+    cbar*q_uz_sz/prop["delta_x"];
+  double lsz = (k - lambda_prime*min(input["lsz"],state["lsz_max"]))/lambda;
+  lsz = min(lsz,state["lsz_max"]);
+
+  // mass ballance on saturated zone
+  double tilde_sz = state["sz"] + ts*(state["lsz"]+lsz)/2 - ts*(input["lsz"]+state["lsz_in"])/2;
+  state["sz"] = max(tilde_sz,0);
+  double q_sz_ex = state["sz"] - tilde_sz;
+  state["lsz"] = lsz;
+  state["lsz_in"] = input["lsz"];
+  output["lsz"] = state["lsz"]*prop["area"];
+
+  // correct stores
+  state["ex"] = state["ex"] + q_rz_ex + q_sz_ex;
+  if( state["sz"] <= 0 ){
+    state["ex"] = state["ex"] + state["uz"];
+    state["uz"] = 0;
+  }
+  
+  //h["output"]=input;
+  //Rcout << "leave hillslope" << "\n";
+};
+
+
 // [[Rcpp::export]]
-List rcpp_dynatop(List hru){
+void rcpp_dynatop(List hru, NumericVector obs, List opt){
+
+  CharacterVector type_list = "mux";
+  //CharacterVector::create(Named("mux","mux"),Named("leaf","leaf"));
   
   Rcout << "started" << "\n";
   int n = hru.size();
-
-  for( int i=0; i<n; i++){
+  int cnt = 0;
+  
+  Rcout << "n=" << n << "\n";
+  for( int i=0; i<n; ++i){
     List h = hru[i];
-    CharacterVector type = h["type"];
+    std::string type = h["type"];
 
+    if( type == "ex" ){
+      f_ex(h,obs);
+      //NumericVector out = h["output"];
+      //out["precip"] = 9.2;
+    }
     
-    Rcout << type << "\n";
+    // mux operator to join data together
+    if( type == "mux" ){
+      //Rcout << type << "\n";
+      f_mux(h,hru);
+    }
+
+    // hillslope type for a dynamic topmodel hillslope
+    if( type == "hillslope" ){
+      std::string lbl = as<std::string>(h["uptree"]);
+      List h_in = hru[lbl];
+      NumericVector input = h_in["output"];
+      f_hillslope(h,input,opt);
+      //Rcout << type << "\n";
+    }
+
+    if( type == "channel" ){
+      std::string lbl = as<std::string>(h["uptree"]);
+      List h_in = hru[lbl];
+      NumericVector input = h_in["output"];
+      f_channel(h,input,opt);
+      //Rcout << type << "\n";
+    }
+    
+    cnt =  cnt + 1;
+    //Rcout << type << "\n";
+
+    // user interupt
+    Rcpp::checkUserInterrupt();
   }
 
-  return hru;
+  Rcout << cnt << "\n";
+  //return hru;
 
 }
+
