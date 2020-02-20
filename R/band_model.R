31ee4214edf542a3272dfb89b7f0ae43da47af1a
Paul Smith <paul@waternumbers.co.uk>
2020-02-20 13:34:12 +0000

    Altered band_model to use both sf and sz bands

--- R/band_model.R
+++ R/band_model.R
@@ -3,7 +3,7 @@
 #' @description take a model and tries to merge the computational bands to produce a simplier description
 #'
 #' @param model a RasterBrick as created by create_model
-#' @param nband number of bands to merge
+#' @param cuts number of bands to merge
 #'
 #' @return a simplified model
 #' @export
@@ -18,12 +18,16 @@ band_model <- function(model, cuts){
     ##hs$class <- hs$split_id
 
     ## create new uids
-    grp <- as.numeric( cut(hs$band,cuts) )
-    if(any(!is.finite(grp))){
+    sz_grp <- as.numeric( cut(hs$sz_band,cuts) )
+    sf_grp <- as.numeric( cut(hs$sf_band,cuts) )
+    if(any(!is.finite(sz_grp)) | any(!is.finite(sf_grp))){
         stop("Cut does not assign each HRU to a new band")
     }
-    grp <- 0.5*(hs$class+grp)*(hs$class+grp+1)+grp # unique new uid by cantor pair
+    # unique new uid by cantor pair
+    grp <- 0.5*(hs$class+sz_grp)*(hs$class+sz_grp+1)+sz_grp 
+    grp <- 0.5*(grp+sf_grp)*(hs$class+sf_grp+1)+sf_grp 
     ugrp <- unique(grp)
+    
     hs[,'new_id'] <- NA
     for(ii in 1:length(ugrp)){
         hs[grp==ugrp[ii],'new_id'] <- ii
@@ -39,16 +43,17 @@ band_model <- function(model, cuts){
         s_bar = as.numeric(by(hs$s_bar*hs$area,hs$new_id,sum)),
         delta_x = as.numeric(by(hs$delta_x,hs$new_id,sum)),
         class = as.numeric(by(hs$class,hs$new_id,unique)),
-        band = as.numeric(by(hs$band,hs$new_id,min)),
-        precip_series="unknown",
-        pet_series="unknown",
-        qex_max="qex_max_default",
+        sz_band = as.numeric(by(hs$sz_band,hs$new_id,min)),
+        sf_band = as.numeric(by(hs$sf_band,hs$new_id,min)),
+        precip="unknown",
+        pet="unknown",
+        qsf_max="qsf_max_default",
         srz_max="srz_max_default",
         srz_0="srz_0_default",
         ln_t0="ln_t0_default",
         m="m_default",
         td="td_default",
-        tex="tex_default",
+        tsf="tsf_default",
         stringsAsFactors=FALSE
     )
     hs2$s_bar <- hs2$s_bar/hs2$area
@@ -61,35 +66,40 @@ band_model <- function(model, cuts){
                       dims=c(max(hs$id),max(hs2$id)))
     A <- Diagonal(max(hs$id),c(model$channel$area,hs$area))
     
-    Dsz <- t(K)%*%model$Dsz%*%A%*%K ## fractions combined with area
+    Dsz <- t(K)%*%model$Fsz%*%A%*%K ## fractions combined with area
     Asz <- t(K)%*%A%*%K # area in each
     Dsz <- Dsz %*% Diagonal(ncol(Asz),1/diag(Asz)) # explicit inverse since diagonal
     
-    Dex <- t(K)%*%model$Dex%*%A%*%K
-    Aex <- t(K)%*%A%*%K # area in each
-    Dex <- Dex %*% Diagonal(ncol(Aex),1/diag(Aex))
+    Dsf <- t(K)%*%model$Fsf%*%A%*%K
+    Asf <- t(K)%*%A%*%K # area in each
+    Dsf <- Dsf %*% Diagonal(ncol(Asf),1/diag(Asf))
     
     ## impose limit can;t drain to same band - but with same area draining out
     tDsz <- colSums(Dsz) # current sum of fractions
-    tDex <- colSums(Dsz) # current sum of fractions
+    tDsf <- colSums(Dsz) # current sum of fractions
 
     ## remove elements not needed
-    for(ii in hs2$band){
-        idx <- hs2$id[hs2$band==ii]
+    for(ii in hs2$sz_band){
+        idx <- hs2$id[hs2$sz_band==ii]
         for(jj in idx){
             Dsz[idx,jj] <- 0
-            Dex[idx,jj] <- 0
         }
     }
-
+    for(ii in hs2$sf_band){
+        idx <- hs2$id[hs2$sf_band==ii]
+        for(jj in idx){
+            Dsf[idx,jj] <- 0
+        }
+    }
+    
     ## standardise
     Dsz <- Dsz %*% Diagonal(ncol(Dsz),tDsz/colSums(Dsz))
-    Dex <- Dex %*% Diagonal(ncol(Dex),tDsz/colSums(Dex))
+    Dsf <- Dsf %*% Diagonal(ncol(Dsf),tDsz/colSums(Dsf))
 
     ## put back into model
     model$hillslope <- hs2
-    model$Dex <- Dex
-    model$Dsz <- Dsz
+    model$Fsf <- Dsf
+    model$Fsz <- Dsz
 
     return(model)
 }
