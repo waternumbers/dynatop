bc99f3dbafc72185248f3977cde4cb61e708520a
Paul Smith <paul@waternumbers.co.uk>
2020-02-27 17:52:39 +0000

    Changed so only one model format seen by user
    Former-commit-id: 014d7ef619245b8b726932b64bd1ff7760e424a6
    Former-commit-id: fdc7289f3980c3a960fd505caca8ea4a60363e9b

---
+++ R/convert_form.R
@@ -0,0 +1,114 @@
+#' Contains functions for converting between the storage form and list form used in simulation
+#'
+#' @description TODO
+#'
+#' @param model a dynamic TOPMODEL model list object
+#' @param initial_recharge Initial recharge to the saturated zone in steady state in m/hr
+#'
+#' @details TODO
+#' 
+#' @return TODO
+#'
+#' @name convert_form
+NULL
+
+#' @rdname convert_form
+## convert a data frame to a storage list
+df_to_store_list <- function(df,type=c("hillslope","channel"),use_states=FALSE){
+    
+    type <- match.arg(type)
+    
+    ## get a list of all variables that should exist
+    vrbl <- required_vrbl(type)
+        
+    ## initialise depending upon type
+    out <- list(attr=list(),
+                input=list(),
+                param=list(),
+                state=list(),
+                flux=list())
+    
+    for(ii in vrbl$attr){
+        out$attr[[ii]] <- unname( df[,ii] )
+    }
+    for(ii in vrbl$input){
+        out$input[[ii]] <-  rep(0,nrow(df))
+    }
+    for(ii in vrbl$param){
+        out$attr[[ii]] <- unname( df[,ii] )
+        out$param[[ii]] <- unname( model$param[ df[,ii] ] )
+    }
+    for(ii in vrbl$state){
+        if(use_states){
+            out$state[[ii]] <- unname( df[,ii] )
+        }else{
+            out$state[[ii]] <- rep(0,nrow(df)) ## initialise all stores and fluxes as 0
+        }
+    }
+    for(ii in vrbl$flux){
+        out$flux[[ii]] <- rep(0,nrow(df)) ## initialise all stores and fluxes as 0
+    }
+    
+    return(out)
+}
+
+
+#' @rdname convert_form
+#' @export
+store_to_sim <- function(model, use_states=FALSE){
+
+    for(ii in c("hillslope","channel")){
+        model[[ii]] <- df_to_store_list(model[[ii]],ii,use_states)
+    }
+
+    ## collapsed matrices as lists
+    ## ith element of list contains the parents (and fractions) for the
+    ## hsu with id = i
+    tmp <- as.matrix(summary(model$Fsf))
+    tmp <- lapply(by(tmp[,2:3],tmp[,1],unique,simplify=FALSE),as.list)
+    model$sf <- rep(list(NULL),max(as.numeric(names(tmp))))
+    model$sf[as.numeric(names(tmp))] <- tmp
+    
+    tmp <- as.matrix(summary(model$Fsz))
+    tmp <- lapply(by(tmp[,2:3],tmp[,1],unique,simplify=FALSE),as.list)
+    model$sz <- rep(list(NULL),max(as.numeric(names(tmp))))
+    model$sz[as.numeric(names(tmp))] <- tmp
+
+    to_keep <- c("hillslope","channel","sz","sf")
+
+    return(model[to_keep])
+}
+
+#' @rdname initialise
+## convert the store back to a table
+sim_to_store <- function(model,with_matrix=TRUE){
+    
+    for(ii in c("hillslope","channel")){
+        model[[ii]] <- cbind( as.data.frame(model[[ii]]$attr),
+                             as.data.frame(model[[ii]]$state))
+    }
+
+    ## recreate matrices
+    tmp <- rep(list(NULL),length(model$sz))
+    for(ii in 1:length(model$sz)){
+        if(length(model$sz[[ii]]$x)>0){
+            tmp[[ii]] <- cbind( i=ii,as.data.frame(model$sz[[ii]]) )
+        }
+    }
+    tmp <- do.call(rbind,tmp)
+    #browser()
+    model$Fsz <- Matrix::sparseMatrix(i=tmp$i,j=tmp$j,x=tmp$x)
+
+    tmp <- rep(list(NULL),length(model$sf))
+    for(ii in 1:length(model$sf)){
+        if(length(model$sf[[ii]]$x)>0){
+            tmp[[ii]] <- cbind( i=ii,as.data.frame(model$sf[[ii]]) )
+        }
+    }
+    tmp <- do.call(rbind,tmp)
+    model$Fsf <- Matrix::sparseMatrix(i=tmp$i,j=tmp$j,x=tmp$x)
+
+    to_keep <- c("hillslope","channel","Fsz","Fsf")
+
+    return(model[to_keep])
+}
--- R/initialise.R
+++ R/initialise.R
@@ -18,169 +18,70 @@ required_vrbl <- function(type=c("hillslope","channel")){
     type <- match.arg(type)
     
     if(type=="hillslope"){
-        out <- list(attr = c("id","atb_bar","s_bar","area","delta_x","sz_band","sf_band"),
-                    series = c("precip","pet"),
-                    input = c("precip","pet","lsf","lsz"),
-                    output = c("et","lsf","lsz"),
-                    param = c("qsf_max","srz_max","srz_0","ln_t0","m","td","tsf"),
-                    state = c("sf","rz","uz","sz","lsz_max"), # stages - not dependent upon time step
-                    flux = c("qex_rz","qrz_ex","qrz_uz","quz_sz","quz_ex","qsz_ex") # fluxes depend on timestep
+        out <- list(attr = c("id","atb_bar","s_bar","area","delta_x","sz_band","sf_band","precip","pet"), # values associated with catchment HSU
+                    param = c("q_sfmax","s_rzmax","s_rz0","ln_t0","m","t_d","t_sf"), # parameter names - also attributes but seperate since handled differently
+                    input = c("p","e_p","l_sf"), # tempory store of input, not needed for next timestep
+                    state = c("s_sf","s_rz","s_uz","s_sz","l_sz_in","l_sz","l_szmax"), # states - required for next timestep
+                    flux = c("q_ex_rz","q_rz_ex","q_rz_uz","q_uz_sz","q_uz_ex","q_sz_ex","e_t","l_sf","l_sz_in_t","Q_minus_t","Q_plus_t") # fluxes within and from the HSU not needed for the next time step
                     )
     }
     if(type=="channel"){
-        out <- list(attr = c("id","area","sz_band","sf_band"),
-                    series = c("precip","pet"),
-                    input = c("precip","pet","lsf","lsz"),
-                    output = c("lch"),
+        out <- list(attr = c("id","area","sz_band","sf_band","precip","pet"),
+                    input = c("p","e_p","l_sf"),
                     param = character(0),
-                    state = c("sch"),
-                    flux = character(0))
+                    state = c("l_sz_in"),
+                    flux = c("s_ch","l_sz_in_t"))
     }
     return(out)
 }
-
-#' @rdname initialise
-## create an empty store
-empty_store <- function(model,type=c("hillslope","channel")){
-    #browser()
-    type <- match.arg(type)
-
-    ## get a list of all variables that should exist
-    vrbl <- required_vrbl(type)
-
-    tmp <- model[[type]]
-    
-    ## initialise depending upon type
-    out <- list(attr=list(),
-                series=list(),
-                input=list(),
-                param=list(),
-                state=list(),
-                flux=list())
-    
-    for(ii in vrbl$attr){
-        out$attr[[ii]] <- unname( tmp[,ii] )
-    }
-    for(ii in vrbl$series){
-        out$series[[ii]] <- unname( tmp[,ii] )
-    }
-    for(ii in vrbl$input){
-        out$input[[ii]] <-  rep(0,nrow(tmp))
-    }
-    for(ii in vrbl$output){
-        out$output[[ii]] <-  rep(0,nrow(tmp))
-    }
-    for(ii in vrbl$param){
-        out$param[[ii]] <- unname( model$param[ tmp[,ii] ] )
-    }
-    for(ii in vrbl$state){
-        out$state[[ii]] <- rep(0,nrow(tmp)) ## initialise all stores and fluxes as 0
-    }
-    for(ii in vrbl$flux){
-        out$flux[[ii]] <- rep(0,nrow(tmp)) ## initialise all stores and fluxes as 0
-    }
-    
-    return(out)
-}
-
 
 #' @rdname initialise
 #' @export
-initialise <- function(model,initial_recharge){
-    #browser()
+initialise <- function(model,initial_recharge, return_sim=FALSE){
+        
     ## check initial discharge
     if( !is.numeric(initial_recharge) | length(initial_recharge) > 1 | any( initial_recharge < 0 ) ){
         stop("Initial discharge should be a single positive numeric value")
     }
-
-    ## #########################################
-    ## Initialise the output
-    mdl <- list()
-
     
-    ## collapsed matrices as lists
-    ## ith element of list contains the parents (and fractions) for the
-    ## hsu with id = i
-    tmp <- as.matrix(summary(model$Fsf))
-    tmp <- lapply(by(tmp[,2:3],tmp[,1],unique,simplify=FALSE),as.list)
-    mdl$sf <- rep(list(NULL),max(as.numeric(names(tmp))))
-    mdl$sf[as.numeric(names(tmp))] <- tmp
-    
-    tmp <- as.matrix(summary(model$Fsz))
-    tmp <- lapply(by(tmp[,2:3],tmp[,1],unique,simplify=FALSE),as.list)
-    mdl$sz <- rep(list(NULL),max(as.numeric(names(tmp))))
-    mdl$sz[as.numeric(names(tmp))] <- tmp
-    
-    ## add the channel hsu
-    mdl$channel <- empty_store(model,"channel")
+    ## convert the model to the correct list type
+    mdl <- store_to_sim(model)
 
-    ## add the hillslope hsu
-    hs <- empty_store(model,"hillslope")
-
-    ## maximum lateral flow from saturated zone
-    hs$state$lsz_max <- exp(hs$param$ln_t0)*hs$attr$s_bar
-
-    ## initialise the root zone
-    hs$state$rz <- pmax( pmin( hs$param$srz_0, 1) ,0 ) * hs$param$srz_max
-
-    ## initialise the saturated zone
-    hs$output$lsz <- hs$input$lsz <- pmin(hs$state$lsz_max,initial_recharge)
-
-    ## compute the deficit
+    ## ###########################################
+    ## Initialise the states
     #browser()
-    gamma <- sum(hs$attr$area*(hs$attr$atb_bar - hs$param$ln_t0))  / sum(hs$attr$area)
-    hs$state$sz <- pmax(0, hs$param$m*(gamma + log(hs$output$lsz)))
+    ## maximum lateral flow from saturated zone
+    mdl$hillslope$state$l_szmax <- exp(mdl$hillslope$param$ln_t0)*mdl$hillslope$attr$s_bar
+    
+    ## initialise the root zone
+    mdl$hillslope$state$s_rz <- pmax( pmin( mdl$hillslope$param$s_rz0, 1) ,0 ) * mdl$hillslope$param$s_rzmax
 
+    mdl$hillslope$state$s_uz[] <- 0
+    
+    ## initialise the saturated zone
+    mdl$hillslope$state$l_sz_in <- mdl$hillslope$state$l_sz <- pmin(mdl$hillslope$state$l_szmax,initial_recharge)
+    mdl$hillslope$state$s_sz <- pmax(0, mdl$hillslope$param$m*(log(mdl$hillslope$state$l_szmax) - log(mdl$hillslope$state$l_sz)))
+    
+    ## initialise the unsaturated zone based on recharge
+    ##mdl$hillslope$state$s_uz <- pmax(0, initial_recharge * mdl$hillslope$param$t_d * mdl$hillslope$state$s_sz)
+    
+    ##saturated zone
+    ## initialise the saturated zone
+    ##mdl$hillslope$state$l_sz_in <- mdl$hillslope$state$l_sz <- pmin(mdl$hillslope$state$l_szmax,initial_recharge)
+    
+    ## compute the deficit
+    ##gamma <- sum(mdl$hillslope$attr$area*(mdl$hillslope$attr$atb_bar - mdl$hillslope$param$ln_t0))  / sum(mdl$hillslope$attr$area)
+    ##mdl$hillslope$state$s_sz <- pmax(0, mdl$hillslope$param$m*(gamma + log(mdl$hillslope$state$l_sz)))
+    
     ## unsaturated storage by inverse of eqn for q_uz in Beven & Wood 1983
-    hs$state$uz <- pmax(0, initial_recharge * hs$param$td * hs$state$sz)
+   
+    ## mdl$hillslope$state$s_uz <- pmax(0, initial_recharge * mdl$hillslope$param$t_d * mdl$hillslope$state$s_sz)
+    
 
-    mdl$hillslope <- hs
-
+    ## convert form back if required
+    if( !return_sim ){
+        mdl <- sim_to_store(mdl)
+    }
+    
     return(mdl)
 }
-
-
-##     ## #########################################
-##     ## Hill slope initialisation
-    
-##     hs <- empty_store(model,"hillslope")
-    
-##     ## initialise root zone based on fraction constrained within 0,1
-##     hs$rz <- pmax( pmin( hs$srz_0, 1) ,0 ) * hs$srz_max
-
-##     ## maximum lateral flow from saturated zone
-##     hs$lsz_max <- exp(hs$ln_t0)*hs$s_bar
-
-    
-##     ## initialise based on steady state of saturated zone given constant recharge from unsturated zone
-##     q_0 <- initial_recharge
-
-##     ## a least squares solution with total outflow equals total inflow
-##     ## to saturated zone
-##     tryCatch({
-##         hs$lsz <- as.numeric( solve( Diagonal(length(hs$id)) - model$Dsz[hs$id,hs$id],
-##                                     rep(q_0,length(hs$id))) )
-##     },error = function(e){
-##         warning("Solution for saturated zone initialisation produced negative or zero flows. Using constant recharge across catchment")
-##         hs$lsz <- rep(q_0,length(hs$id))
-##     })
-
-##     ## threshold at max
-##     hs$lsz <- pmin( hs$lsz , hs$lsz_max )
-
-##     ## compute the deficit
-##     gamma <- sum(hs$area*(hs$atb_bar - hs$ln_t0))  / sum(hs$area)
-##     hs$sz <- hs$m*(gamma + log(hs$lsz))
-##     hs$sz <- pmax(0,hs$sz)
-
-##     ## unsaturated storage by inverse of eqn for q_uz in Beven & Wood 1983
-##     hs$uz <- pmax(0, rep(q_0,length(hs$id)) * hs$td * hs$sz, na.rm=TRUE)
-
-##     cmdl$hillslope <- hs
-    
-##     ## ########################################
-##     ## initialise the channel
-##     cmdl$channel <- empty_store(model,"channel")
-    
-##     return(cmdl)
-## }
