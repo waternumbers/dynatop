bb3fcd5ef82b5b5afe92ff3b33a412d64c46ff50
Paul Smith <paul@waternumbers.co.uk>
2020-03-04 17:04:43 +0000

    Altered conversion to speed up

--- R/convert_form.R
+++ R/convert_form.R
@@ -65,12 +65,12 @@ store_to_sim <- function(model, use_states=FALSE){
     ## ith element of list contains the parents (and fractions) for the
     ## hsu with id = i
     tmp <- as.matrix(summary(model$Fsf))
-    tmp <- lapply(by(tmp[,2:3],tmp[,1],unique,simplify=FALSE),as.list)
+    tmp <- lapply(by(tmp,tmp[,1],unique,simplify=FALSE),as.list)
     model$sf <- rep(list(NULL),ncol(model$Fsf))
     model$sf[as.numeric(names(tmp))] <- tmp
     
     tmp <- as.matrix(summary(model$Fsz))
-    tmp <- lapply(by(tmp[,2:3],tmp[,1],unique,simplify=FALSE),as.list)
+    tmp <- lapply(by(tmp,tmp[,1],unique,simplify=FALSE),as.list)
     model$sz <- rep(list(NULL),ncol(model$Fsz))
     model$sz[as.numeric(names(tmp))] <- tmp
 
@@ -92,8 +92,7 @@ sim_to_store <- function(model,with_matrix=TRUE){
     tmp <- rep(list(NULL),length(model$sz))
     for(ii in 1:length(model$sz)){
         if(length(model$sz[[ii]]$x)>0){
-            tmp[[ii]] <- cbind( i=ii,
-                               as.data.frame(model$sz[[ii]],stringsAsFactors=FALSE) )
+            tmp[[ii]] <- as.data.frame(model$sz[[ii]],stringsAsFactors=FALSE)
         }
     }
     tmp <- do.call(rbind,tmp)
@@ -103,8 +102,7 @@ sim_to_store <- function(model,with_matrix=TRUE){
     tmp <- rep(list(NULL),length(model$sf))
     for(ii in 1:length(model$sf)){
         if(length(model$sf[[ii]]$x)>0){
-            tmp[[ii]] <- cbind( i=ii,
-                               as.data.frame(model$sf[[ii]],stringsAsFactors=FALSE) )
+            tmp[[ii]] <- as.data.frame(model$sf[[ii]],stringsAsFactors=FALSE)
         }
     }
     tmp <- do.call(rbind,tmp)
