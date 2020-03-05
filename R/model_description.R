66ae27d397fd9072712faedc815b8643551f68ca
Paul Smith <paul@waternumbers.co.uk>
2020-03-05 21:57:38 +0000

    More changes to model desscription

--- R/model_description.R
+++ R/model_description.R
@@ -5,31 +5,31 @@
 #' @param type the description of which model table to return
 #'
 #' @details TODO
-#' 
+#'
 #' @return A data frame descriping the properties of one of the tables found within the model
-#' 
+#'
 model_description <- function(type=c("hillslope","channel","point_inflow","gauge"),include_states=FALSE,include_tmp=FALSE){
     type <- match.arg(type)
-    
+
     if(type=="hillslope"){
         out <- data.frame(name = c("id","atb_bar","s_bar","area","delta_x","sz_band","sf_band", # attributes associated with catchment HSU
                                    "precip","pet", # names of input series
                                    "q_sfmax","s_rzmax","s_rz0","ln_t0","m","t_d","t_sf", # parameter names
                                    "s_sf","s_rz","s_uz","s_sz","sum_l_sz_in","l_sz","l_szmax", # states
-                                   "p","e_p","l_sf","q_sf_rz","q_rz_sf","q_rz_uz","q_uz_sz","q_uz_sf","q_sz_sf","e_t","l_sf","vol_l_sz_in","Q_minus_t","Q_plus_t","Q_minus_tDt"), ## tempory stores not needed for next timestep
+                                   "p","e_p","e_t","l_sf","q_sf_rz","q_rz_sf","q_rz_uz","q_uz_sz","q_uz_sf","q_sz_sf","e_t","l_sf","sum_l_sz_in_t","l_sz_t","Q_minus_t","Q_plus_t","Q_minus_tDt"), ## tempory stores not needed for next timestep
                           role = c(rep("attribute",7),
                                    rep("data_series",2),
                                    rep("parameter",7),
                                    rep("state",7),
-                                   rep("tmp",15)),
+                                   rep("tmp",16)),
                           type = c(rep("numeric",7),
                                    rep("character",2),
                                    rep("character",7),
                                    rep("numeric",7),
-                                   rep("numeric",15)
+                                   rep("numeric",16)
                                    ),
                           stringsAsFactors=FALSE)
-        
+
     }
     if(type=="channel"){
         out <- data.frame(name= c("id","area","sz_band","sf_band","length","next_id", # states
@@ -72,6 +72,6 @@ model_description <- function(type=c("hillslope","channel","point_inflow","gauge
     if(!include_tmp){
         out <- out[out$role!="tmp",,drop=FALSE]
     }
-    
+
     return(out)
 }
