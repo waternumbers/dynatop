4f58706afac613b654577c7afa86764489faada6
Paul Smith <paul@waternumbers.co.uk>
2020-03-06 17:22:19 +0000

    Added build file
    Former-commit-id: 2b7b0c8d0c2288034966bbc3ccfb1f9e68b9e968
    Former-commit-id: 722dc69815051584b212bd5875dbc37d921695ee

--- make_dynatop.R
+++ make_dynatop.R
@@ -14,6 +14,7 @@ pkgdown::clean_site(pacPath)
 pkgdown::build_site(pacPath)
 #pkgdown::build_article("Time_Delay_Channel_Routing",pacPath)
 
+###########################################################################
 ## This converts the exdata for Swindale into the data object used in the examples.
 devtools::load_all(pacPath)
 model <- readRDS( system.file("extdata","Swindale.rds",package="dynatop") )
@@ -28,46 +29,53 @@ Swindale <- list(model=model,obs=obs)
 save("Swindale",file="./dynatop/data/Swindale.rda")
 
 
-#### Converting the old test_catchments to new ones
+
+##################################
+## Bodging back together the test catchments
 rm(list=ls())
-load("./test_catchment.rda")
+pacPath <- "./dynatop"
+devtools::load_all(pacPath)
 
-simple_hillslope <- test_catchment$model
-simple_hillslope$hillslope$id <- 3:10
-simple_hillslope$hillslope$sf_band <- simple_hillslope$hillslope$sz_band <- 1:8
-simple_hillslope$hillslope$qsf_max <- "qsf_max_default"
-simple_hillslope$hillslope$delta_x <- 1
-tmp <- names(simple_hillslope$hillslope)
-tmp <- gsub("tex","tsf",tmp)
-tmp <- gsub("_input","",tmp)
-names(simple_hillslope$hillslope) <- tmp
+data("test_catchments")
 
-tmp <- names(simple_hillslope$channel)
-tmp <- gsub("tex","tsf",tmp)
-tmp <- gsub("_input","",tmp)
-names(simple_hillslope$channel) <- tmp
-simple_hillslope$channel$sf_band <- simple_hillslope$channel$sz_band <- 9:10
+## change hillslope names
+tmp <- names(test_catchments$simple_hillslope$hillslope)
+tmpr <- c("qsf_max"="q_sfmax",
+          "srz_max"="s_rzmax",
+          "srz_0"="s_rz0",
+          "td"="t_d",
+          "tsf"="t_sf")
+for(ii in names(tmpr)){
+    tmp <- gsub(ii,tmpr[ii],tmp)
+}
+names(test_catchments$simple_hillslope$hillslope) <- tmp
 
-tmp <- names(simple_hillslope$point_inflow)
-tmp <- gsub("channel_","",tmp)
-names(simple_hillslope$point_inflow) <- tmp
+chng <- data.frame(t = "hillslope",
+                   v="sz_band",
+                   c="numeric",
+                   stringsAsFactors=FALSE)
+chng[1,] <- c("hillslope","sz_band","numeric")
+chng[2,] <- c("hillslope","sf_band","numeric")
+chng[3,] <- c("channel","sz_band","numeric")
+chng[4,] <- c("channel","sf_band","numeric")
+chng[5,] <- c("channel","id","integer")
+chng[6,] <- c("channel","next_id","integer")
+chng[7,] <- c("gauge","id","integer")
+chng[8,] <- c("point_inflow","id","integer")
 
-tmp <- names(simple_hillslope$gauge)
-tmp <- gsub("channel_","",tmp)
-names(simple_hillslope$gauge) <- tmp
-
-simple_hillslope$param['qsf_max_default'] <- Inf
-
-simple_hillslope$Fex <- simple_hillslope$Fsat <- simple_hillslope$Wex <- simple_hillslope$Wsat <- NULL
-simple_hillslope$Fsf <- simple_hillslope$Fsz <-
-    Matrix::sparseMatrix(i=c(4:10,1,2),j=c(3:10,10),x=c(rep(1,8),0))
-
-obs <- test_catchment$obs
-obs$point_1 <- obs$point_2 <- 0
+for(ii in 1:nrow(chng)){
+    tmp <- test_catchments$simple_hillslope[[ chng[ii,'t'] ]][[ chng[ii,'v'] ]]
+    tmp <- switch(chng[ii,'c'],
+                  "numeric" = as.numeric(tmp),
+                  "integer" = as.integer(tmp),
+                  stop("missing type"))
+    test_catchments$simple_hillslope[[ chng[ii,'t'] ]][[ chng[ii,'v'] ]] <- tmp
+}
 
 
-test_catchments <- list(simple_hillslope=simple_hillslope,
-                        obs = obs)
+
+check_model(test_catchments$simple_hillslope)
+
 
 save(test_catchments,file="./dynatop/data/test_catchments.rda")
 
@@ -94,20 +102,20 @@ data("Swindale")
 #model <- band_model(Swindale$model,5)
 model <- Swindale$model
 ## to fix elsewhere
-model$channel$sz_band <- model$channel$sf_band <- 1e4
+
 
 
 model$hillslope[,'precip'] <- model$channel[,'precip'] <- "Rainfall"
 model$hillslope[,'pet'] <- model$channel[,'pet'] <- "PET"
 
-model$param <- c(q_sfmax_default=0,
+mdlb$param <- c(q_sfmax_default=Inf,
                  m_default=0.0063,
-                 ln_t0_default=7.46,
+                 ln_t0_default=3.46,
                  s_rz0_default=0.98,
                  s_rzmax_default=0.1,
-                 v_ch_default=3000,
-                 t_d_default=8*60*60,
-                 t_sf_default=3.6e+05
+                 v_ch_default=0.8,
+                 t_d_default=8,#1,#8*60*60,
+                 t_sf_default=3#.6e+05
                  )
 
 mdlb <- band_model(model,5)
@@ -115,24 +123,30 @@ mdlb$hillslope[,'precip'] <- mdlb$channel[,'precip'] <- "Rainfall"
 mdlb$hillslope[,'pet'] <- mdlb$channel[,'pet'] <- "PET"
 
 obs <- Swindale$obs
-obs$PET[] <- 0
+#obs$PET[] <- 0
 #obs$Rainfall[] <- 0
 
 model0 <- initialise(model,0.000001)
 v0 <- hillslope_volume(model0)
-qhat <- dynatop(model,obs,0.000001)
-vend <- hillslope_volume(qhat$model)
-v0 + (sum(obs$Rainfall)*sum(model$hillslope$area)) + (sum(obs$Rainfall)*sum(model$channel$area)) - (sum(qhat$channel_input)*15*60) - vend
+qhat3 <- dynatop(model0,obs,sim_time_step=300,use_states=TRUE)
+vend <- hillslope_volume(qhat3$model)
+v0 + (sum(obs$Rainfall)*sum(model$hillslope$area)) + (sum(obs$Rainfall)*sum(model$channel$area)) - (sum(qhat3$channel_input)*15*60) - vend
 
 mdlb0 <- initialise(mdlb,0.000001)
 vb0 <- hillslope_volume(mdlb0)
-qhatb <- dynatop(mdlb0,obs,use_states=TRUE)
+qhatb <- dynatop(mdlb0,obs,sim_time_step=3600,use_states=TRUE,mass_check=TRUE) #0.000001)
+qhatb2 <- dynatop(qhatb$model,obs,sim_time_step=3600,use_states=TRUE,mass_check=TRUE)
 vbend <- hillslope_volume(qhatb$model)
 
 vb0 + (sum(obs$Rainfall)*sum(mdlb$hillslope$area)) + (sum(obs$Rainfall)*sum(mdlb$channel$area)) - (sum(qhatb$channel_input)*15*60) - vbend
 
-x11();plot(cbind(Swindale$obs[,'Flow'],rowSums(qhatb$channel_input)))
+x11();plot(cbind(Swindale$obs[,'Flow'],rowSums(qhatb$channel_input),
+                 rowSums(qhatb2$channel_input)))
 
-                 rowSums(qhat$channel_input),rowSums(qhatb$channel_input)))
+chhat <- time_delay_routing(mdlb,qhatb$channel_input)
+x11();plot(cbind(Swindale$obs[,'Flow'],rowSums(qhatb$channel_input),chhat))
+plot(chhat)
+
+
 
 
