6e72827f6fa9694c1d5f830493de30053905aa17
Paul Smith <paul@waternumbers.co.uk>
2022-09-22 17:16:45 +0100

    more testing

--- build_scripts/test_soln/test.R
+++ build_scripts/test_soln/test.R
@@ -1,42 +1,129 @@
-## test the celerity based solution with additional dispersion
+## TVDish solution
 rm(list=ls())
-n <- 1100
-s <- rep(NA,n)
-cel <- 10
-disp <- 000*100/2
-qin <- rep(1,n)
-qin[200:500] <- 0
-r <- rep(0.1,n)
-qout <- rep(NA,n)
-qav <- rep(NA,n)
-s[1] <- 10
-qout[1] <- qin[1]
-Dt <- 1
-Dx <- 100
+graphics.off()
 
-K <- cel*Dt/Dx
-eta <- min(K,0.5 - (disp/(cel*Dx)))
+## constant velocity
 
-qav[1] <- eta*qin[1] + (1-eta)*qout[1]
-
-qqout <- qout
-qqav <- qav
-ss <- s
-
-for(ii in 2:n){
-    
-    ss[ii] <- max(0, ss[ii-1] + ( Dt / (1+K-eta) )*(qin[ii] + (1-eta)*r[ii] - qqav[ii-1]) )
-    qqout[ii] <- (ss[ii-1] + Dt*(qin[ii]+r[ii]) - ss[ii])/Dt
-    qqav[ii] <- eta*qin[ii] + (1-eta)*qqout[ii]
-    
-    qout[ii] <- ( qav[ii-1] + (K-eta)*qin[ii] + K*r[ii] ) / ( 1-eta+K )
-    s[ii] <- s[ii-1] + Dt*(qin[ii] - qout[ii] + r[ii])
-    qav[ii] <- eta*qin[ii] + (1-eta)*qout[ii]
+fs <- function(qi,qo){
+    Dx <- 100
+    v <- 1.3
+    eta <- 0.5
+    s <- (Dx/v)*( (eta*qi) + (1-eta)*qo) ## check if this way round in text
+    return(s)
 }
 
-par(mfrow=c(2,1))
-plot(s); lines(ss)
-plot(qout); lines(qqout)
+fF <- function(qo,qi,s0,vi,q0,Dt,theta){
+    vo <- Dt*( (1-theta)*q0 + theta*qo )
+    return( fs(qi,qo) - s0 - vi + vo )
+}
+
+
+ftheta <- function(qo,qi,s0,vi,q0,Dt){
+    ##(s0 + vi - Dt*q0 - fs(ai,x,Dx) ) / ( Dt * (fq(x)-q0) )
+    #browser()
+    (fs(qi,qo) - s0 - vi + Dt*q0) / (Dt*(q0 - qo))
+}
+
+n <- 1100
+s <- vin <- vout <- qout <- rep(NA,n)
+qin <- rep(1,n)
+qin[200:500] <- 20
+r <- rep(0,n)
+Dt <- 90
+
+## initialise at steady state
+qout[1] <- qin[1] + r[1]
+vin[1] <- qin[1]*Dt
+vout[1] <- qout[1]*Dt
+s[1] <- fs(qin[1],qout[1])
+
+
+
+## loop
+for(ii in 2:n){
+    vin[ii] <- Dt*0.5*(qin[ii]+qin[ii-1])
+    ##if(ii==200){ browser()}
+    ## range of aout for TVD solution
+    rng <- range( c(qin[ii],qout[ii-1],qin[ii-1]) )
+    #rng <- c(0,5)
+    frng <- c( fF(rng[1],qin[ii],s[ii-1],vin[ii],qout[ii-1],Dt,1),
+              fF(rng[2],qin[ii],s[ii-1],vin[ii],qout[ii-1],Dt,1) )
+
+   
+    if(rng[1]==rng[2]){
+        qout[ii] <- rng[1]
+    }else{        
+        omega <- 0.5
+        if( qin[ii] > qout[ii-1] ){
+            omega <- max(qin[ii-0:1])
+            omega <- ftheta( omega, qin[ii],s[ii-1],vin[ii],qout[ii-1],Dt)
+        }
+        if( qin[ii] < qout[ii-1] ){
+            omega <- min(qin[ii-0:1])
+            omega <- ftheta( omega, qin[ii],s[ii-1],vin[ii],qout[ii-1],Dt)
+        }
+        print(omega)
+        omega <- min(1,max(0.5, omega))
+        ##omega <- 0.5
+        rng <- c(-0.1,300)
+        frng <- c( fF(rng[1],qin[ii],s[ii-1],vin[ii],qout[ii-1],Dt,omega),
+                  fF(rng[2],qin[ii],s[ii-1],vin[ii],qout[ii-1],Dt,omega) )
+        qout[ii] <- uniroot(fF, rng,
+                            qi=qin[ii], s0=s[ii-1],
+                            vi=vin[ii],q0=qout[ii-1],
+                            Dt=Dt,theta=omega)$root
+    }
+    s[ii] <- fs( qin[ii],qout[ii] )
+    vout[ii] <- s[ii-1] + vin[ii] - s[ii]
+}
+
+                
+    
+par(mfrow=c(3,1))
+plot(s)
+plot(qout); lines(qin)
+plot(vout); lines(vin)
+
+## ## test the celerity based solution with additional dispersion
+## rm(list=ls())
+## graphics.off()
+## n <- 1100
+## s <- rep(NA,n)
+## cel <- 1
+## disp <- 0*100/2
+## qin <- rep(1,n)
+## qin[200:500] <- 0
+## r <- rep(0.1,n)
+## qout <- rep(NA,n)
+## qav <- rep(NA,n)
+## s[1] <- 10
+## qout[1] <- qin[1]
+## Dt <- 1
+## Dx <- 100
+
+## K <- cel*Dt/Dx
+## eta <- min(K,0.5 - (disp/(cel*Dx)))
+
+## qav[1] <- eta*qin[1] + (1-eta)*qout[1]
+
+## qqout <- qout
+## qqav <- qav
+## ss <- s
+
+## for(ii in 2:n){
+    
+##     ss[ii] <- max(0, ss[ii-1] + ( Dt / (1+K-eta) )*(qin[ii] + (1-eta)*r[ii] - qqav[ii-1]) )
+##     qqout[ii] <- (ss[ii-1] + Dt*(qin[ii]+r[ii]) - ss[ii])/Dt
+##     qqav[ii] <- eta*qin[ii] + (1-eta)*qqout[ii]
+    
+##     qout[ii] <- ( qav[ii-1] + (K-eta)*qin[ii] + K*r[ii] ) / ( 1-eta+K )
+##     s[ii] <- s[ii-1] + Dt*(qin[ii] - qout[ii] + r[ii])
+##     qav[ii] <- eta*qin[ii] + (1-eta)*qout[ii]
+## }
+
+## par(mfrow=c(2,1))
+## plot(s); lines(ss)
+## plot(qout); lines(qqout)
 
 
 ## ## Classic formulation
